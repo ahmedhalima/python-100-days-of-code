74518c9da8bc60eff77d6f673c2ce3d79948cdbe
Ahmed Maher Halima <phpcodertop@gmail.com>
2026-09-08 03:27:49 +0300

    finish day 17

--- day17/main.py
+++ day17/main.py
@@ -9,3 +9,6 @@ for question in question_data:
 quiz = QuizBrain(questionBank)
 while quiz.still_has_questions():
     quiz.next_question()
+
+print('You\'ve completed the quiz.')
+print(f'Your final score is {quiz.score}/{quiz.question_number}')
