7b30ec2cf1b3adab739fd22f2344d4a66394d2db
Ahmed Maher Halima <phpcodertop@gmail.com>
2026-09-10 02:01:07 +0300

    day20 create the snake game and make it move

---
+++ day20/main.py
@@ -0,0 +1,29 @@
+from turtle import Screen
+from snake import Snake
+import time
+
+screen = Screen()
+screen.bgcolor('black')
+screen.setup(width=600, height=600)
+screen.title('Snake Game')
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(fun=snake.up, key='Up')
+screen.onkey(fun=snake.down, key='Down')
+screen.onkey(fun=snake.left, key='Left')
+screen.onkey(fun=snake.right, key='Right')
+
+
+screen.update()
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+
+screen.exitonclick()
