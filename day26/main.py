dc07daf4dd7bbe0cbd44bb680562d37b2dd73524
Ahmed Maher Halima <phpcodertop@gmail.com>
2026-09-15 02:46:50 +0300

    day26 nato phonetic

--- day26/main.py
+++ day26/main.py
@@ -4,7 +4,7 @@ import pandas
 csv_list = pandas.read_csv('nato_phonetic_alphabet.csv')
 phonetic_list = {row.letter:row.code for (key, row) in csv_list.iterrows()}
 
-user_input = input('Enter your name\n')
-user_input_list = {alpha.upper():phonetic_list[alpha.upper()] for alpha in user_input}
+user_input = input('Enter your name\n').upper()
+user_input_list = {alpha:phonetic_list[alpha] for alpha in user_input}
 
 print(user_input_list)
